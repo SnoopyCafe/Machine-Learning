8d6a8775d927faf197f32e84f78c9afff8cb52f7
SnoopyCafe <presto.williams@gmail.com>
2016-10-17 14:01:16 -0400

    File upload

---
+++ ClassifyNB.py
@@ -0,0 +1,18 @@
+def classify(features_train, labels_train, features_test, labels_test):
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+
+
+    ### your code goes here!
+    from sklearn.naive_bayes import GaussianNB
+    from sklearn.metrics import accuracy_score
+
+    clf = GaussianNB()
+    clf.fit(features_train, labels_train)
+    pred = clf.predict(features_test)
+    print ( "accuracy {:.3f}".format(accuracy_score(pred, labels_test)))
+    ### accuracy = no. of points classified correctly / all points (in test set)
+    return clf
+    # print(clf.predict([[-0.8, -1]]))
